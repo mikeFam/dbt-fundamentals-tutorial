d795bc4cf53e6c9f8adb06484ae8f561b69115c5
Michael Pham <michael.pham@servian.com>
2022-12-05 23:59:54 +0000

    rename files

--- models/customers_daily_summary.sql
+++
@@ -1,7 +0,0 @@
-select 
-    {{ dbt_utils.surrogate_key(['customer_id', 'order_date']) }} as id,
-    customer_id,
-    order_date,
-    count(*)
-from {{ ref('stg_orders') }}
-group by 1, 2, 3
--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,4 +1,5 @@
 {%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
 with payments as (
     select * from {{ ref('stg_payments') }}
 ),
@@ -8,7 +9,7 @@ pivoted as (
         order_id,
         {%- for method in payment_methods %}
         sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount {%- if not loop.last %},{% endif -%}
-        {%- endfor -%}
+        {%- endfor %}
 
     from payments
     where status = 'success'
